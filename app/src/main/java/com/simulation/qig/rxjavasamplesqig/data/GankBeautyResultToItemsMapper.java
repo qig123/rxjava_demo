27179d292c87df2f6123039538780f673a258b78
qig-PC\qig <qiugxyz2@gmail.com>
2018-01-31 23:45:39 +0800

    add  mapfragment

---
+++ app/src/main/java/com/simulation/qig/rxjavasamplesqig/data/GankBeautyResultToItemsMapper.java
@@ -0,0 +1,8 @@
+package com.simulation.qig.rxjavasamplesqig.data;
+
+/**
+ * Created by qig on 2018/1/31.
+ */
+
+public class GankBeautyResultToItemsMapper {
+}
---
+++ app/src/main/java/com/simulation/qig/rxjavasamplesqig/module/map_2/MapFragment.java
@@ -0,0 +1,8 @@
+package com.simulation.qig.rxjavasamplesqig.module.map_2;
+
+/**
+ * Created by qig on 2018/1/31.
+ */
+
+public class MapFragment {
+}
